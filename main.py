2d7e6acfa65d1ba12af627e87a47ffcfa8216651
Ryan Murphy <toadgit76@gmail.com>
2024-12-09 13:32:29 -0500

    Worked on it a little.

--- main.py
+++ main.py
@@ -12,5 +12,22 @@ def generate_room(keyroom_found):
         room = rooms[random.randint(0, len(rooms)-1)]
 
     descriptor = random.choice(room_descriptors)
+    
+    r = random.randint(1,10)
 
-    room_title = "You entered a " + size + " " + descriptor + " " + room + "."
+    if (r >= 7):
+    	attr = "This room has a monster!"
+    room_title = "You entered a " + size + " " + descriptor +  " " + room + ".\n" + attr 
+    roomspecs.append(room_title)
+    
+    if (size == "small"):
+    	loot_size = 1
+    elif (size == "medium"):
+    	loot_size = 2
+    elif (size == "large"):
+    	loot_size = 3
+    else:
+    	loot_size = 4
+	
+    for i in range(loot_size):
+    	
