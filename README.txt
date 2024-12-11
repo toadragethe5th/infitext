2cbe73eaec9021a4a4aba75e33477424b674590e
Ryan Murphy <toadgit76@gmail.com>
2024-12-11 13:52:15 -0500

    Reorganization.

--- README.txt
+++
@@ -1,3 +0,0 @@
-INFITEXT
-
-Simple text-based RPG written in python for a school project.
--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import random
 import math
 from vars import *
 
+# https://www.w3schools.com/python/ref_func_any.asp
+# HOLY CRAP
+
 
 def generate_room(keyroom_found):
     roomspecs = []
@@ -14,12 +17,13 @@ def generate_room(keyroom_found):
     descriptor = random.choice(room_descriptors)
     
     r = random.randint(1,10)
-
-    if (r >= 7):
-    	attr = "This room has a monster!"
-    room_title = "You entered a " + size + " " + descriptor +  " " + room + ".\n" + attr 
-    roomspecs.append(room_title)
     
+    roomspecs.append(size)
+    roomspecs.append(descriptor)
+    roomspecs.append(room_title)
+    roomspecs.append(attr)
+    
+    """
     if (size == "small"):
     	loot_size = 1
     elif (size == "medium"):
@@ -28,6 +32,32 @@ def generate_room(keyroom_found):
     	loot_size = 3
     else:
     	loot_size = 4
-	
-    for i in range(loot_size):
-    	
+    """
+    return roomspecs
+
+def main():
+	running = True # Pygame style loop
+	coords = (0,0)
+	try:
+		file = open("gamesave")
+
+	while (running):
+		directive = input("Enter what you want to do (or HELP): ")
+		directive = directive.lower()
+		if directive == "help":
+			print("""north - move north
+				 south - move south
+				 east  - move east
+				 west  - move west
+				 attack - attack an enemy
+				 scavenge - scavenge for stuff
+				 list - list inventory
+				 equip - equip an item
+				 exit  - leave (why would you do this?)
+				 loadpath - Changes where the game is saved and the filename.
+				 The game autosaves on exit.
+
+				 """
+		elif directive == "north":
+			coords[1] += 1
+			loadRoom(roomDict, 
